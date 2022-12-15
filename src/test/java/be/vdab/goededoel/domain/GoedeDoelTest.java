bf2ee1f2522dc871d7eba7ddefa8608599877308
Bidur <thapabidur86@gmail.com>
2022-12-15 10:22:49 +0100

    JaCOCO added in the pom

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -38,5 +38,7 @@ class GoedeDoelTest {
 	void doelVerschiltVanEenObjectMetEenAnderType(){
 		assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
 	}
+	@Test void gelijkeDoelGevenHebbenDezelfdeHashCode()
+	{ assertThat(doel).hasSameHashCodeAs(new GoedeDoel(NAAM)); }
 
 }
